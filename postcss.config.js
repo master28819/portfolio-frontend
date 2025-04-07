af5c32c02a7d929a6ac9508f0461de82a5b35c3b
MASTER OF C <136145964+master28819@users.noreply.github.com>
2025-04-07 15:59:33 +0000

    Initial Commit - Portfloio Setup

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-import tailwindcss from '@tailwindcss/postcss7-compat';
-import autoprefixer from 'autoprefixer';
-
-export default {
-  plugins: [
-    tailwindcss(),
-    autoprefixer(),
-  ],
-};
--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router"; // Import the router
-
-const app = createApp(App);
-app.use(router); // Use Vue Router in the app
-app.mount("#app");
